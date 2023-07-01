1fa7605522fbe2713ceeca7bbb0f5d57f97c3c2d
davidbonilla <93225583+dbonilla1988@users.noreply.github.com>
2023-07-01 02:25:34 -0700

    Token Deployment

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,5 +1,6 @@
 require("@nomiclabs/hardhat-waffle");
-require("dotenv").config(); 
+require("dotenv").config();
+const privateKeys = process.env.PRIVATE_KEYS || "";
 
 // This is a sample Hardhat task. To learn how to create your own go to
 // https://hardhat.org/guides/create-task.html
@@ -19,7 +20,11 @@ task("accounts", "Prints the list of accounts", async (taskArgs, hre) => {
  */
 module.exports = {
   solidity: "0.8.9",
-  networks: { 
-    localhost: {}
+  networks: {
+    localhost: {},
+    goerli: {
+      url: `https://eth-goerli.g.alchemy.com/v2/${process.env.ALCHEMY_API_KEY}`,
+      accounts: privateKeys.split(','),
+    },
   },
 };
